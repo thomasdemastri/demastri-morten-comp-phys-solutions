51cd0aa2de1c55486f823e2b7a763789c39d8ee7
thomasdemastri <tmastri@dev-10.com>
2022-09-01 15:14:58 -0400

    Started experimenting with c++

---
+++ setup/source/HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+
+A Very simple setup program to ensure that my C++ 
+environment is up and running :) 
+
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"Hello World!\n";	
+}
