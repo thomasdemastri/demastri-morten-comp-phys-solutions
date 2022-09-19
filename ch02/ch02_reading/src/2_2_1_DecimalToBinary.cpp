169a0faafddb0c11fcf7de1a6e300b3af0718cde
tdemastri <tdemastri@dev-10.com>
2022-09-19 18:58:03 -0400

    fixed some bugs with the chapter two reading decimal to binary program

--- ch02/ch02_reading/src/2_2_1_DecimalToBinary.cpp
+++ ch02/ch02_reading/src/2_2_1_DecimalToBinary.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * A program that converts decimal numbers to a binary represenatation
+ * A program that converts decimal integers to a binary represenatation
  *
  */
 
@@ -22,25 +22,29 @@ main
 
 	int index;
 
-	std::cout<<decimal<<" can be represented with ";
+	// initialize all values of the array to zero
+	for(int j = 0;j<32;j++){
+		digits[j]=0;
+	}
 
+	std::cout << decimal << " can be represented with ";
+ 
 	for(index = 0; index<32; index++){
 
-		if(decimal==0){
+		if(decimal==0 && index>0){
 			break;
 		}
 
-		digits[index] = decimal%2;
+		digits[31-index] = decimal%2;
 		decimal /= 2;
 	}
 
-	std::cout<<index<<" bits!" << std::endl;
-
+	std::cout << index << " bits!" << std::endl;
 
 
 	std::cout<<"(";
 
-	for(int i=31; i>-1;i--){
+	for(int i=32-index;i<32;i++){
 
 		std::cout<<digits[i];
 
